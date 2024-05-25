f27af14f58a9734fc67c6e1852d3d72b96ae4985
Rahulji2002 <rahulgoraitiltoria33@gmail.com>
2024-05-25 22:51:54 +0530

    updated_home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -42,41 +42,107 @@ export default function Home() {
           <div className="hero-search-filter">
             <p>Location</p>
             <div ref={(el) => (locationRef.current[0] = el)}>
-              <input value={locationText} type="text" placeholder="Where are you going?" onChange={(e) => setLocationText(e.target.value)} onClick={() => setIsLocationOptionsOpen(true)} />
-              <div className={`hero-search-options ${!isLocationOptionsOpen && "hidden"}`}>
+              <input
+                value={locationText}
+                type="text"
+                placeholder="Where are you going?"
+                onChange={(e) => setLocationText(e.target.value)}
+                onClick={() => setIsLocationOptionsOpen(true)}
+              />
+              <div
+                className={`hero-search-options ${
+                  !isLocationOptionsOpen && "hidden"
+                }`}
+              >
                 <p>Popular nearby destinations</p>
-                <SearchOptionItem city="Kolkata" state="India" setLocationText={setLocationText} setIsLocationOptionsOpen={setIsLocationOptionsOpen} />
-                <SearchOptionItem city="Delhi" state="India" setLocationText={setLocationText} setIsLocationOptionsOpen={setIsLocationOptionsOpen} />
-                <SearchOptionItem city="Mumbai" state="India" setLocationText={setLocationText} setIsLocationOptionsOpen={setIsLocationOptionsOpen} />
-                <SearchOptionItem city="Kerala" state="India" setLocationText={setLocationText} setIsLocationOptionsOpen={setIsLocationOptionsOpen} />
-                <SearchOptionItem city="Bangaluru" state="India" setLocationText={setLocationText} setIsLocationOptionsOpen={setIsLocationOptionsOpen} />
+                <SearchOptionItem
+                  city="Kolkata"
+                  state="India"
+                  setLocationText={setLocationText}
+                  setIsLocationOptionsOpen={setIsLocationOptionsOpen}
+                />
+                <SearchOptionItem
+                  city="Delhi"
+                  state="India"
+                  setLocationText={setLocationText}
+                  setIsLocationOptionsOpen={setIsLocationOptionsOpen}
+                />
+                <SearchOptionItem
+                  city="Mumbai"
+                  state="India"
+                  setLocationText={setLocationText}
+                  setIsLocationOptionsOpen={setIsLocationOptionsOpen}
+                />
+                <SearchOptionItem
+                  city="Kerala"
+                  state="India"
+                  setLocationText={setLocationText}
+                  setIsLocationOptionsOpen={setIsLocationOptionsOpen}
+                />
+                <SearchOptionItem
+                  city="Bangaluru"
+                  state="India"
+                  setLocationText={setLocationText}
+                  setIsLocationOptionsOpen={setIsLocationOptionsOpen}
+                />
               </div>
             </div>
           </div>
           <div className="hero-search-filter">
             <p>Type</p>
             <div ref={(el) => (locationRef.current[1] = el)}>
-              <input value={typeText} type="text" placeholder="Flat, Paying Guest" onChange={(e) => setTypeText(e.target.value)} onClick={() => setIsTypeOptionsOpen(true)} readOnly />
-              <div className={`hero-search-options ${!isTypeOptionsOpen && "hidden"}`}>
+              <input
+                value={typeText}
+                type="text"
+                placeholder="Flat, Paying Guest"
+                onChange={(e) => setTypeText(e.target.value)}
+                onClick={() => setIsTypeOptionsOpen(true)}
+                readOnly
+              />
+              <div
+                className={`hero-search-options ${
+                  !isTypeOptionsOpen && "hidden"
+                }`}
+              >
                 <p>Popular nearby destinations</p>
-                <SearchOptionItem setTypeText={setTypeText} setIsTypeOptionsOpen={setIsTypeOptionsOpen} />
+                <SearchOptionItem
+                  setTypeText={setTypeText}
+                  setIsTypeOptionsOpen={setIsTypeOptionsOpen}
+                />
               </div>
             </div>
           </div>
           <div className="hero-search-filter">
             <p>Price Range</p>
             <div ref={(el) => (locationRef.current[2] = el)}>
-              <input value={priceText} type="text" placeholder="Set your budget" onChange={(e) => setPriceText(e.target.value)} onClick={() => setIsPriceOptionsOpen(true)} readOnly />
-              <div className={`hero-search-options ${!isPriceOptionsOpen && "hidden"}`}>
+              <input
+                value={priceText}
+                type="text"
+                placeholder="Set your budget"
+                onChange={(e) => setPriceText(e.target.value)}
+                onClick={() => setIsPriceOptionsOpen(true)}
+                readOnly
+              />
+              <div
+                className={`hero-search-options ${
+                  !isPriceOptionsOpen && "hidden"
+                }`}
+              >
                 <p>Popular nearby destinations</p>
-                <SearchOptionPrice setPriceText={setPriceText} setIsPriceOptionsOpen={setIsPriceOptionsOpen} />
+                <SearchOptionPrice
+                  setPriceText={setPriceText}
+                  setIsPriceOptionsOpen={setIsPriceOptionsOpen}
+                />
               </div>
             </div>
           </div>
           <a
-            href={`/search?location=${locationText.substring(0, locationText.indexOf(","))}&type=${typeText === "" ? "Flat, Paying Guest" : typeText}&price=${priceText
-              .replace(/,/g, "")
-              .match(/[0-9]*[0-9]/g)}`}
+            href={`/search?location=${locationText.substring(
+              0,
+              locationText.indexOf(",")
+            )}&type=${
+              typeText === "" ? "Flat, Paying Guest" : typeText
+            }&price=${priceText.replace(/,/g, "").match(/[0-9]*[0-9]/g)}`}
           >
             <button className="btn">Search</button>
           </a>
@@ -85,8 +151,8 @@ export default function Home() {
       {/* Popular Section */}
       <section className="popular-container">
         <div>
-          <h1 className="popular-header">Popular PG's</h1>
-          <p>Checkouts ours best PG&apos;s and find your best room that suits you.</p>
+          <h1 className="popular-header">Popular PG/Flat</h1>
+          <p>Browse Our Best PG's/Flat's and Select Your Perfect Room.</p>
         </div>
         <div className="popular-items">
           <PopularItem background="./images/Home/popular-1.png" />
@@ -99,7 +165,7 @@ export default function Home() {
       <section className="popular-container">
         <div className="offers-text">
           <h1 className="popular-header">Special Offers</h1>
-          <p>Get best offers</p>
+          <p>Avail Exclusive Deals</p>
         </div>
         <div className="offers-items">
           <OfferItem background="./images/Home/offer-1.png" />
@@ -111,34 +177,59 @@ export default function Home() {
       <section className="video-container">
         <div>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-            galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            Browse our selection of top-rated PGs and flats to find the perfect
+            room that fits your lifestyle. Our carefully curated options offer
+            comfort, convenience, and affordability, ensuring you have a
+            seamless living experience. Each property is equipped with essential
+            amenities and is located in prime areas for easy access to
+            everything you need. Start your search today and discover your ideal
+            living space with us!
           </p>
           <h3>Wonderful Room Experience</h3>
-          <h3>Balcony Room Tour</h3>
+          <h3>With Balcony, Modern Bathrooms, Room Decorations, and etc(s)</h3>
         </div>
       </section>
       {/* Feature Section */}
       <section className="feature-container">
-        <h1>We Provides Latest Properties For Our Valuable Clients</h1>
+        <h1>
+          We ensure a seamless experience, matching you with your dream property
+        </h1>
         <div>
           <div>
-            <img style={{ transform: "translate(20rem,0)" }} src="/images/Home/feature-1.png" alt="" />
-            <img style={{ transform: "translate(21rem,13rem)" }} src="/images/Home/feature-3.png" alt="" />
-            <img style={{ transform: "translate(10rem,5rem)", width: "18rem" }} src="/images/Home/feature-2.png" alt="" />
+            <img
+              style={{ transform: "translate(20rem,0)" }}
+              src="/images/Home/feature-1.png"
+              alt=""
+            />
+            <img
+              style={{ transform: "translate(21rem,13rem)" }}
+              src="/images/Home/feature-3.png"
+              alt=""
+            />
+            <img
+              style={{ transform: "translate(10rem,5rem)", width: "18rem" }}
+              src="/images/Home/feature-2.png"
+              alt=""
+            />
           </div>
           <div className="feature-text">
             <div>
               <h2>Budget Friendly</h2>
-              <p>We will find you the best PG according to your budget</p>
+              <p>Cost-effective choices for your ideal living situation.</p>
             </div>
             <div>
               <h2>Prime Location</h2>
-              <p>We will find you the best PG according to your budget</p>
+              <p>
+                Discover Convenience and Comfort in Prime Locations! Easy to
+                access your needs.
+              </p>
             </div>
             <div>
               <h2>Trust Worthy</h2>
-              <p>We will find you the best PG according to your budget</p>
+              <p>
+                Reliable, Honest, and Dependable: Your Trusted Choice for
+                Quality Service.
+              </p>
             </div>
           </div>
         </div>
