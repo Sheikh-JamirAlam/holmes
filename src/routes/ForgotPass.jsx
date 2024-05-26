7cbcf4b352c9c3d2517b65491835ce83693e8c6b
Sheikh-JamirAlam <alamjamir15@gmail.com>
2024-05-26 22:34:53 +0530

    forgot pass

--- src/routes/ForgotPass.jsx
+++ src/routes/ForgotPass.jsx
@@ -2,14 +2,21 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { CrossInCircle } from "../components/Icons";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import { CrossInCircle, PhEye, PhEyeClosed } from "../components/Icons";
 import "../styles/Login.css";
 
 export default function ForgotPass() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [otp, setOtp] = useState("");
+  const [isPassHidden, setIsPassHidden] = useState(true);
+  const [isPassError, setIsPassError] = useState(false);
+  const [pass, setPass] = useState("");
+  const [cpass, setCpass] = useState("");
   const [stage, setStage] = useState("email");
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (Cookies.get("auth")) {
@@ -17,11 +24,16 @@ export default function ForgotPass() {
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   async function handleClick() {
     try {
-      const res = await axios.post("http://localhost:8081/api/otp/request-otp", null, { params: { email } });
+      const res = await axios.post("http://localhost:8080/api/otp/request-otp", null, { params: { email } });
       if (res.data === "OTP sent to email!") {
         setStage("otp");
+        setOpen(true);
       }
     } catch (error) {
       console.error(error);
@@ -30,12 +42,33 @@ export default function ForgotPass() {
 
   async function handleConfirmOTP() {
     try {
-      const res = await axios.post("http://localhost:8081/api/otp/verify-otp", null, { params: { email, otp } });
+      const res = await axios.post("http://localhost:8080/api/otp/verify-otp", null, { params: { email, otp } });
       if (res.data === "OTP verified successfully!") {
         setStage("password");
       }
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  async function handlePassChange() {
+    if (cpass !== pass) {
+      setIsPassError(true);
+    } else {
+      try {
+        const res = await axios.post("http://localhost:8080/api/user/updatepass", {
+          userEmail: email,
+          userPass: pass,
+        });
+        if (res.data === "Update successful") {
+          setOpen(true);
+          setTimeout(() => {
+            navigate("/");
+          }, 1500);
+        }
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
 
@@ -74,17 +107,44 @@ export default function ForgotPass() {
         )}
         {stage === "password" && (
           <>
-            <p>Enter the OTP you just received</p>
+            <p>OTP verified! Enter new Password</p>
             <div className="input-container">
               <div className="input">
-                <input type="number" placeholder="Enter OTP" value={otp} onChange={(e) => setOtp(e.target.value)} />
+                <input
+                  type={isPassHidden ? "password" : "text"}
+                  placeholder="Enter password"
+                  value={pass}
+                  onChange={(e) => {
+                    setPass(e.target.value);
+                    setIsPassError(false);
+                  }}
+                />
+                <span onClick={() => setIsPassHidden(!isPassHidden)}>{isPassHidden ? <PhEyeClosed /> : <PhEye />}</span>
               </div>
-              <button className="button" onClick={handleClick}>
+              <div className={`input ${isPassError && "pass-error"}`}>
+                <input
+                  type={isPassHidden ? "password" : "text"}
+                  placeholder="Confirm password"
+                  value={cpass}
+                  onChange={(e) => {
+                    setCpass(e.target.value);
+                    setIsPassError(false);
+                  }}
+                />
+                <span onClick={() => setIsPassHidden(!isPassHidden)}>{isPassHidden ? <PhEyeClosed /> : <PhEye />}</span>
+              </div>
+              <button className="button" onClick={handlePassChange}>
                 Confirm
               </button>
             </div>
           </>
         )}
+        <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success" variant="filled" sx={{ width: "100%" }}>
+            {stage === "otp" ? "Email has been sent!" : ""}
+            {stage === "password" ? "Successfully Updated Password" : ""}
+          </Alert>
+        </Snackbar>
       </div>
     </section>
   );
