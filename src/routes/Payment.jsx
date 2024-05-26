ed7754f5a804da8ab3c7950f562f57cdf3f99172
Sheikh-JamirAlam <alamjamir15@gmail.com>
2024-05-26 15:20:18 +0530

    home

--- src/routes/Payment.jsx
+++ src/routes/Payment.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Backdrop from "@mui/material/Backdrop";
 import "../styles/Payment.css";
 
 export default function Payment() {
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+
   const handleClose = () => {
     setOpen(false);
+    navigate("/");
   };
-  const handleSubmit = (e) => {
+  const handleOpen = () => {
     setOpen(true);
+    document.getElementById("checkmark").classList.add("checkmark-animate");
+    setTimeout(() => {
+      handleClose();
+    }, 2000);
   };
 
   return (
@@ -28,53 +36,29 @@ export default function Payment() {
 
                 <div className="payment-inputBox">
                   <span>Full Name :</span>
-                  <input
-                    className="payment-input"
-                    type="text"
-                    placeholder="John Deo"
-                  />
+                  <input className="payment-input" type="text" placeholder="John Deo" />
                 </div>
                 <div className="payment-inputBox">
                   <span>Email :</span>
-                  <input
-                    className="payment-input"
-                    type="email"
-                    placeholder="1234@gmail.com"
-                  />
+                  <input className="payment-input" type="email" placeholder="1234@gmail.com" />
                 </div>
                 <div className="payment-inputBox">
                   <span>Address :</span>
-                  <input
-                    className="payment-input"
-                    type="text"
-                    placeholder="Street - Locality"
-                  />
+                  <input className="payment-input" type="text" placeholder="Street - Locality" />
                 </div>
                 <div className="payment-inputBox">
                   <span>City :</span>
-                  <input
-                    className="payment-input"
-                    type="text"
-                    placeholder="Mumbai"
-                  />
+                  <input className="payment-input" type="text" placeholder="Mumbai" />
                 </div>
 
                 <div className="payment-flex">
                   <div className="payment-inputBox">
                     <span>State :</span>
-                    <input
-                      className="payment-input"
-                      type="text"
-                      placeholder="India"
-                    />
+                    <input className="payment-input" type="text" placeholder="India" />
                   </div>
                   <div className="payment-inputBox">
                     <span>Zip code :</span>
-                    <input
-                      className="payment-input"
-                      type="text"
-                      placeholder="123 456"
-                    />
+                    <input className="payment-input" type="text" placeholder="123 456" />
                   </div>
                 </div>
               </div>
@@ -111,17 +95,13 @@ export default function Payment() {
                 </div>
               </div>
             </div>
-            <button className="payment-submit-btn" onClick={handleSubmit}>
+            <button className="payment-submit-btn" onClick={handleOpen}>
               Proceed to Checkout
             </button>
-            <Backdrop
-              sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-              open={open}
-              onClick={handleClose}
-            >
+            <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open} onClick={handleClose}>
               <div className="payment-success-container">
                 <div className="checkmark-circle">
-                  <div className="checkmark"></div>
+                  <div id="checkmark" className="checkmark"></div>
                 </div>
                 <h1 className="paymentS-h1">Payment Successful!</h1>
                 <p className="paymentS-p">Thank you for your purchase.</p>
